b1f7f8d2033bbb8ca756c6e308b9aaae8f94597a
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2025-06-27 13:26:17 +0000

    Go backend Practice

--- server/main.go
+++ server/main.go
@@ -1,26 +1,15 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/AbdulRahman-04/Go_Backend_Practice/config"
-	"github.com/AbdulRahman-04/Go_Backend_Practice/utils"
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 func main(){
-	// DB IMPORT
-	utils.DbConnect()
- 
 	router := gin.Default()
 
-	router.GET("/", func (c *gin.Context){
+	router.GET("/", func (c*gin.Context){
 		c.JSON(200, gin.H{
-			"msg" : "Hello from gin",
+			"msg": "HELLO IN GIN",
 		})
 	})
 
-	router.Run(fmt.Sprintf(":%d", config.AppConfig.Port))
-	 
-
+	router.Run(":4050")
 }
