35b9600fc60c14247fb6f42f22854202147ebb12
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2025-06-29 08:37:59 +0000

    go practice backend

--- server/controllers/public/users.go
+++ server/controllers/public/users.go
@@ -2,9 +2,9 @@ package public
 
 import (
 	"context"
-	"crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"math/rand"
 	"strings"
 	"time"
 
@@ -36,108 +36,149 @@ func UserSignUp(c *gin.Context){
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	// create a struct for storing user input inside it
+	// create a struct that stores user input
 	type UserSignUp struct {
 		UserName string `json:"userName" form:"userName"`
-		Email string `form:"email" json:"email"`
+		Email string `json:"email" form:"email"`
 		Password string `json:"password" form:"password"`
 		Phone string `json:"phone" form:"phone"`
 		Age int `json:"age" form:"age"`
 		Language string `json:"language" form:"language"`
 	}
 
-	// store thsi struct in var and bind into json
+	// create a var and give type as struct
 	var inputUser UserSignUp
 
+	// bind it into json first 
 	if err := c.ShouldBindJSON(&inputUser); err != nil {
        c.JSON(400, gin.H{
-		"msg": "Invalid request",
+		"msg": "invalid req",
 	   })
 	   return
 	}
 
 	// validations 
-	if inputUser.UserName == "" || inputUser.Email == "" || inputUser.Password == "" || inputUser.Phone == "" || inputUser.Age == 0 || inputUser.Language == "" {
+	if inputUser.UserName == ""|| inputUser.Email == "" || inputUser.Password == "" || inputUser.Phone == "" || inputUser.Age == 0 || inputUser.Language == "" {
 		c.JSON(400, gin.H{
-			"msg": "Please input all fields",
+			"msg": "fill all fields",
 		})
 		return
 	}
 	if !strings.Contains(inputUser.Email, "@"){
 		c.JSON(400, gin.H{
-			"msg": "invalid email",
+			"msg": "Invalid email",
 		})
 		return
 	}
 	if len(inputUser.Password) < 6 {
 		c.JSON(400, gin.H{
-			"msg": "6 digits toh rkh",
+			"msg": "atleast 6",
 		})
 		return
 	}
 	if len(inputUser.Phone) < 10 {
 		c.JSON(400, gin.H{
-			"msg": "must be 10",
+			"msg": "must be atleast 10",
 		})
 		return
 	}
 
-	// check if user exusts in db
-	count, err := userCollection.CountDocuments(ctx, bson.M{"email" : inputUser.Email})
+	// check duplicate
+	count, err := userCollection.CountDocuments(ctx, bson.M{"email": inputUser.Email})
 	if err != nil {
-		c.JSON(500, gin.H{
-			"msg": "db error",
+		c.JSON(400, gin.H{
+			"msg": "invalid",
 		})
 		return
 	}
-
 	if count > 0 {
-		c.JSON(404, gin.H{
+		c.JSON(400, gin.H{
 			"msg": "user already exists",
 		})
 		return
 	}
 
-	// hash the password 
+	// hash the pass 
 	hashPass, _ := bcrypt.GenerateFromPassword([]byte(inputUser.Password), 10)
 	emailToken := generateToken(8)
 	phoneToken := generateToken(8)
 
-	// create a var for pushing into db
+	// create model var
 	var user models.User
 
 	user.UserName = inputUser.UserName
-	user.Email = inputUser.Email
+    user.Email = inputUser.Email
 	user.Password = string(hashPass)
-	user.Phone = inputUser.Phone
 	user.Age = inputUser.Age
+	user.Phone = inputUser.Phone
 	user.Language = inputUser.Language
 	user.UserVerified.Email = false
 	user.UserVerifyToken.Email = emailToken
 	user.UserVerifyToken.Phone = phoneToken
 
-	// send email func 
+	// send email for verify 
 	go func() {
 		emailData := utils.EmailData{
-			From: "team insta",
+			From: "Tea, insa",
 			To: inputUser.Email,
-			Subject: "email verify",
-			Html: fmt.Sprintf(`<a href="%s/api/public/user/emailverify/%s"></a>`, config.AppConfig.URL, emailToken ),
+			Html: fmt.Sprintf(`<a href="%s/api/public/user/emailverify/%s"></a>`, config.AppConfig.URL, emailToken),
 		}
-		_ = utils.SendEmail(emailData)
+		_  = utils.SendEmail(emailData); 
 	}()
 
 	_, err = userCollection.InsertOne(ctx, user)
-
 	if err != nil {
 		c.JSON(400, gin.H{
-			"msg": "Error while creating user",
+			"msg": "err",
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"msg": "user created bro verify ur email",
+	})
+}
+
+func EmailVerify(c *gin.Context){
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	token := c.Param("token")
+
+	// find usre with this tokem
+	var user models.User
+
+	err := userCollection.FindOne(ctx, bson.M{"userVerifyToken.email": token}).Decode(&user)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"msg": "invalid token",
 		})
 		return
 	}
 
-	c.JSON(200, gin.H{
-		"msg": "user created✅, verify ur email",
-	})
+	if user.UserVerified.Email {
+		c.JSON(200, gin.H{
+			"msg": "user already verified",
+		})
+		return
+	}
 
+	// update 
+	update := bson.M{
+		"$set": bson.M{
+			"userVerfied.email": true,
+			"userVerifyToken": nil,
+			"updated_at": time.Now(),
+		}}
+
+
+	_, err = userCollection.UpdateByID(ctx, user.ID, update)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"msg": "error while uodating",
+		})
+		return
+	}	
+	c.JSON(200, gin.H{
+		"msg": "email verified✅",
+	})
 }
